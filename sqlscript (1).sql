642d5e58dd895fc3cd811e97fd786c78be739e9c
Shivansh7800 <shivansh20609@gmail.com>
2024-11-08 13:24:21 +0530

    Add files via upload

---
+++ sqlscript (1).sql
@@ -0,0 +1,17 @@
+REM   Script: Session 01
+REM   SQL PRACTICAL
+
+CREATE TABLE department ( 
+    Dept_no VARCHAR(10), 
+    D_name VARCHAR(10), 
+    D_loc VARCHAR(10) 
+);
+
+CREATE TABLE employee( 
+    EMP_no INT, 
+    EMP_name VARCHAR(10), 
+    job VARCHAR(10), 
+    Manager INT, 
+    Salary INT 
+);
+
